8970a1bbccadc3c460d7457403ed5ffaa3d69907
Facundo Casal <93745699+facundocasal@users.noreply.github.com>
2022-05-26 12:33:26 -0300

    returns pedidos

--- 1-class/class.js
+++ 1-class/class.js
@@ -6,35 +6,34 @@ class Usuario {
         this.mascotas = []
     }
     getFullName (){
-        console.log(`el nombre del usuario es ${this.nombre} ${this.apellido}`)
+        return `el nombre del usuario es ${this.nombre} ${this.apellido}`
     }
     addMascotas(masc){
         this.mascotas.push(masc)
     }
     countMascotas(){
-        console.log(`el usuario tiene ${this.mascotas.length} mascotas`)
+        return this.mascotas.length
     }
     addBook(libro , aut){
         this.libros.push({nombre: libro , autor: aut})
     }
     getBookNames(){
-        console.log(`los libros del usuario son :`)
         const listaLibros = []
         this.libros.map(n=>listaLibros.push(n.nombre))
-        console.log(listaLibros)
+        return listaLibros
     }
 }
 // declaro el usuario 
 const facundo = new Usuario("facundo", "casal")
 // retorno el nombre del usuario
-facundo.getFullName()
+console.log(facundo.getFullName())
 // agrego las mascotas del usuario
 facundo.addMascotas("uma")
 facundo.addMascotas("cereza")
 // muestro la cantidad de mascotas que tiene el usuario 
-facundo.countMascotas()
+console.log(facundo.countMascotas())
 // agrego 2 libros al usuario 
 facundo.addBook("el principito","Antoine de Saint-Exupéry")
 facundo.addBook("cien años de soledad", "Gabriel Garcia Marquez")
 // muestro la lista de los nombres de sus libros 
-facundo.getBookNames()
+console.log(facundo.getBookNames())
