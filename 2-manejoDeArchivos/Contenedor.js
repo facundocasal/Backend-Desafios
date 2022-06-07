15a30acc2d7a20093641adaeaf68b5d832f21f5b
Facundo Nahuel Casal <fcasal92@gmail.com>
2022-06-06 21:11:13 -0300

    agregado de express y servidor

--- 2-manejoDeArchivos/Contenedor.js
+++ 2-manejoDeArchivos/Contenedor.js
@@ -88,18 +88,44 @@ class Contenedor {
 
 const newFile = new Contenedor("./productos.txt")
 
-newFile.save({ title: "cartuchera", price: 100, thumbnail: "https://d3ugyf2ht6aenh.cloudfront.net/stores/891/147/products/15222071-151595990d47d4f35b16467701309837-1024-1024.jpg" }).then(res => {
-    console.log(res)
-    newFile.save({ title: "lapiz", price: 300, thumbnail: "https://papeleria24h.files.wordpress.com/2019/03/punta-lapiz-staedtler-tradition-110.jpg?w=982" }).then(res => {
-        console.log(res)
-        newFile.save({ title: "carpeta", price: 500, thumbnail: "https://www.rioshopdeco.com.ar/6534-large_default/carpeta-pp-tonalizada-escolar-3x40-azul-art-5401.jpg" }).then(res => {
-            console.log(res)
-            // newFile.getAll().then(res => console.log(res))
-            // newFile.getbyId(5).then(res => console.log (res))
-            // newFile.deleteById(2)
-            // newFile.deleteAll()
-        })
-    })
+// newFile.save({ title: "cartuchera", price: 100, thumbnail: "https://d3ugyf2ht6aenh.cloudfront.net/stores/891/147/products/15222071-151595990d47d4f35b16467701309837-1024-1024.jpg" }).then(res => {
+//     console.log(res)
+//     newFile.save({ title: "lapiz", price: 300, thumbnail: "https://papeleria24h.files.wordpress.com/2019/03/punta-lapiz-staedtler-tradition-110.jpg?w=982" }).then(res => {
+//         console.log(res)
+//         newFile.save({ title: "carpeta", price: 500, thumbnail: "https://www.rioshopdeco.com.ar/6534-large_default/carpeta-pp-tonalizada-escolar-3x40-azul-art-5401.jpg" }).then(res => {
+//             console.log(res)
+
+//         })
+//     })
+// })
+
+
+async function cargarProductos (){
+    await newFile.save({ title: "cartuchera", price: 100, thumbnail: "https://d3ugyf2ht6aenh.cloudfront.net/stores/891/147/products/15222071-151595990d47d4f35b16467701309837-1024-1024.jpg" })
+    await newFile.save({ title: "lapiz", price: 300, thumbnail: "https://papeleria24h.files.wordpress.com/2019/03/punta-lapiz-staedtler-tradition-110.jpg?w=982" })
+    await newFile.save({ title: "carpeta", price: 500, thumbnail: "https://www.rioshopdeco.com.ar/6534-large_default/carpeta-pp-tonalizada-escolar-3x40-azul-art-5401.jpg" })
+}
+
+cargarProductos()
+
+const express = require("express")
+const app = express()
+const puerto = 8080
+
+app.get(`/`, (req , res)=>{
+    res.send("hola soy home")
+})
+app.get(`/productos`, (req , res)=>{
+    newFile.getAll().then(i=> res.json(i))
+})
+app.get(`/productosRandom`, (req , res)=>{
+    let numAleatorio = (Math.floor(Math.random() * (4 - 0)))
+    newFile.getbyId(numAleatorio)
+    .then(i=>{ 
+        res.json(i)
+        console.log(numAleatorio)})
 })
 
-
+app.listen(puerto , ()=>{
+    console.log(`servidor escuchando puerto: ${puerto}`)
+})
